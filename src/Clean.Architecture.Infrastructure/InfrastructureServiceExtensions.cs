c2de86b760a391792e1115ff364acb361346e11d
秉虎 <s96016641@gmail.com>
2025-08-25 17:40:39 +0800

    Initial commit

---
+++ src/Clean.Architecture.Infrastructure/InfrastructureServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Clean.Architecture.Core.Interfaces;
+using Clean.Architecture.Core.Services;
+using Clean.Architecture.Infrastructure.Data;
+using Clean.Architecture.Infrastructure.Data.Queries;
+using Clean.Architecture.UseCases.Contributors.List;
+
+
+namespace Clean.Architecture.Infrastructure;
+public static class InfrastructureServiceExtensions
+{
+  public static IServiceCollection AddInfrastructureServices(
+    this IServiceCollection services,
+    ConfigurationManager config,
+    ILogger logger)
+  {
+    string? connectionString = config.GetConnectionString("SqliteConnection");
+    Guard.Against.Null(connectionString);
+
+    services.AddScoped<EventDispatchInterceptor>();
+
+    services.AddDbContext<AppDbContext>((provider, options) =>
+    {
+      var eventDispatchInterceptor = provider.GetRequiredService<EventDispatchInterceptor>();
+      options.UseSqlite(connectionString);
+      options.AddInterceptors(eventDispatchInterceptor);
+    });
+
+    services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>))
+           .AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>))
+           .AddScoped<IListContributorsQueryService, ListContributorsQueryService>()
+           .AddScoped<IDeleteContributorService, DeleteContributorService>();
+
+
+    logger.LogInformation("{Project} services registered", "Infrastructure");
+
+    return services;
+  }
+}
